35e06cf64d44329c138516598fd1a908b6fefdde
rajijamu <38112554+rajijamu@users.noreply.github.com>
2018-11-21 17:59:12 +0300

    Функция определения координат
    Функция определения следующих  координат. Добавлены комментарии

--- coordinate.cpp
+++ coordinate.cpp
@@ -1,10 +1,11 @@
+//расчет следующей координаты
 #include "info.h"
 
 struct coord nextCoordinate(coord coordNow, int prevChangeTime, int timeNow, int speed, float angle)
 {
-	coord next;
-	int t = timeNow - prevChangeTime;
-	next.y = coordNow.y + (t*speed*(sin(atan(angle))));
-	next.x = coordNow.x + (t*speed*(sin(1.5708 - atan(angle))));
-	return next;
+	coord next;		
+	int t = timeNow - prevChangeTime;									//время изменения 
+	next.y = coordNow.y + (t*speed*(sin(atan(angle))));					//измененная y-координата
+	next.x = coordNow.x + (t*speed*(sin(1.5708 - atan(angle))));		//измененная х-координата
+	return next;														//возвращаем новые координаты
 }
