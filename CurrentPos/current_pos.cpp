1191e5b65ae7d1cbf888050fcbbc205d0d9ba936
MoonlightDD <suljaev.sasha@rambler.ru>
2018-11-21 19:09:17 +0300

    CurPos

---
+++ CurrentPos/current_pos.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "WindowsProject1.h"
+
+
+class  Field
+{
+    int widthField = const(10);          
+    public:
+
+}
+
+class Platform
+{
+	int time_last_change, current_width, speed, current_pos;
+	void  InfoWithField();
+	{
+		widthField = current_width = const(10);      //запрос информации о ширине поля у класса поле
+	}
+	void ChekMove                                  //Проверка на то двигался ли предмет
+
+		void UpdatePhysicsPlatform()                //движение предмета
+	{
+
+		while (ChekMove = 1; int ChekMove = 1; a++)     //цикл с проверкой на движение предмета и выдачей текущей позиции
+		{
+			last_time_change = current_time - last_time
+				current_pos = speed * time_last_change
+		}
+	}
+}
