990a17338822ab52425c2916d5d1073765f07523
rajijamu <ravimail40gmail.com>
2018-11-25 16:47:59 +0300

    Обновленная функция координат
    Изменена под стать новой функции угла.

--- ball/coordinate.cpp
+++ ball/coordinate.cpp
@@ -4,8 +4,8 @@
 struct coord nextCoordinate(coord coordNow, int prevChangeTime, int timeNow, int speed, float angle)
 {
 	coord next;		
-	int t = timeNow - prevChangeTime;									//время изменения 
-	next.y = coordNow.y + (t*speed*(sin(atan(angle))));					//измененная y-координата
-	next.x = coordNow.x + (t*speed*(sin(1.5708 - atan(angle))));		//измененная х-координата
-	return next;														//возвращаем новые координаты
+	int t = timeNow - prevChangeTime;					//время изменения 
+	next.y = coordNow.y + (t*speed*(sin(angle)));		//измененная y-координата
+	next.x = coordNow.x + (t*speed*(cos(angle)));		//измененная х-координата
+	return next;										//возвращаем новые координаты
 }
