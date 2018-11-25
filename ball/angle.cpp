806029e1255e3c6cb6a81c5429ef170f5269b959
rajijamu <ravimail40gmail.com>
2018-11-25 16:46:58 +0300

    Обновлена функция угла
    Теперь передается угол в радианах вместо тангенса угла.

--- ball/angle.cpp
+++ ball/angle.cpp
@@ -6,10 +6,15 @@ float angleChange(float angFw, float angOfObst)
 	float angBack;		
 	srand(time(NULL));				//для случайного изменения угла при столкновении с платформой
 	if (angOfObst == 0) {			//при столкновении с верхней/нижней границей поля
-		angBack = (-1)*angFw;		//отражение в обратную сторону по y, по х движение сохраняется
+		angBack = 2*PI - angFw;
 	}
-	else {														//отражение от платформ
-		angBack = (-1)*angFw + ((rand() % 1200 + 1000)/1000);	//угол отражения случайно немного меняется
+	else {												//отражение от платформ, угол случайно немного меняется
+		case ((angFw >= 0) && (angFw<=(PI))):			//движение вверх
+			angBack = PI - angFw + ((rand() % 1200 + 1000)/10000);
+			break;
+		case ((angFw >= PI) && (angFw<=(2*PI))):		//движение вниз 
+			angBack = 3*PI - angFw + ((rand() % 1200 + 1000)/10000);
+			break;
 	}
 	return angBack;					//возврат нового угла движения
 }
